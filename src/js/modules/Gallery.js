57099b8347fc0c8d8bf4c810140f1c8648f7f77b
Liam <liam.pesser@gmail.com>
2021-01-15 01:46:13 +0100

    Mise en place des boutons de Gallery + toggle classe '.active'
    Boutons '.previous', '.next', '.play' et '.stop'

--- src/js/modules/Gallery.js
+++ src/js/modules/Gallery.js
@@ -8,12 +8,14 @@ export default class Gallery {
     this.sliderListEl;
     this.menuListEl;
     this.index = 0;
-    this.intervalTime = 3000;
+    this.timer = null;
+    this.isPlaying = false;
+    this.intervalTime = 2000;
     this.images = []; // Pour transformer les objets en type images et leur mettre des méthodes
     this.loadImages(data.images); // Lancement de la méthode de chargement des données
     this.template = galleryTemplate;
     this.galleryRender();
-    this._displaySLide();
+    this._displaySlide();
   }
 
   /**
@@ -34,6 +36,7 @@ export default class Gallery {
       // Le DOM de Gallery existe pour le navigateur
 
       // Activation des éléments dynamiques
+      this._activateButtons();
       this.menuRender();
       this.sliderRender();
   }
@@ -46,7 +49,7 @@ export default class Gallery {
       // Activation des boutons du menu
       image.menuEl.querySelector('a').onclick = (e) => {
         this.index = image.id - 1;
-        this._displaySLide();
+        this._displaySlide();
       }
     }
   }
@@ -60,10 +63,78 @@ export default class Gallery {
     }
   }
 
-  _displaySLide () {
+  // ACTIVATION DES BOUTONS ----------
+  _activateButtons () {
+    // Bouton previous
+    this.el.querySelector('.previous').onclick = (e) => {
+      this._previous();
+    }
+    // Bouton next
+    this.el.querySelector('.next').onclick = (e) => {
+      this._next();
+    }
+    // Bouton play
+    this.el.querySelector('.play').onclick = (e) => {
+      this._play();
+    }
+    // Bouton stop
+    this.el.querySelector('.stop').onclick = (e) => {
+      this._stop();
+    }
+  }
+
+  // AFFICHAGE DE L'IMAGE DU SLIDER ----------
+  _displaySlide () {
     this.h1 = this.el.querySelector('.slider-menu h1');
     this.sliderListEl.style.left = "-" + this.index + "00%";
     this.h1.innerText = this.images[this.index].alt;
+  }
+
+  // AFFICHAGE DE L'IMAGE PRECEDENTE ----------
+  _previous () {
+    this.index === 0
+    ? this.index = this.images.length - 1
+    : this.index -= 1;
+    this._displaySlide();
+  }
+
+  // AFFICHAGE DE L'IMAGE SUIVANTE ----------
+  _next () {
+    this.index + 1 < this.images.length
+    ? this.index += 1
+    : this.index =0;
+    this._displaySlide();
+  }
+
+  // DEFILEMENT DES IMAGES SELON UN INTERVALLE ----------
+  _play() {
+    this.isPlaying = true;
+    this._toggleActiveClass();
+    this.timer === null
+      ? (this.timer = setInterval(() => {
+          this._next();
+        }, this.intervalTime))
+      : this._stop();
+  }
+
+  // ARRET DU DEFILEMENT DES IMAGES ----------
+  _stop () {
+    this.isPlaying = false;
+    this._toggleActiveClass();
+    clearInterval(this.timer);
+    this.timer = null;
+  }
+
+  // TOGGLE DE LA CLASSE ACTIVE DES BOUTONS PLAY ET STOP ----------
+  _toggleActiveClass () {
+    if (this.isPlaying === true) {
+      this.el.querySelector('.play').classList.add('active');
+      this.el.querySelector('.stop').classList.remove('active');
+    }
+    else {
+      this.el.querySelector('.play').classList.remove('active');
+      this.el.querySelector('.stop').classList.add('active');
+    }
   }
 
 }
